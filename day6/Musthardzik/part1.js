f67d2179e1cc9d501468d28d218156df263d6d16
MustHardzik <banishedsanguine@wp.pl>
2023-12-06 19:25:18 +0100

    mh - day 6 part 1 part 2 added

---
+++ day6/Musthardzik/part1.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(__dirname + "/data.txt", { encoding: "utf-8" });
+const [time, distance] = input.split("\n")
+const [name1, value1] = time.split(": ")
+const [name2, value2] = distance.split(": ")
+
+  let times = value1.replace("\r", "").trim().replaceAll("", "").split(" ").filter(item => item > 0)
+  let distances = value2.replace("\r", "").trim().replaceAll("", "").split(" ").filter(item => item > 0)
+  let multi = 1;
+    for (let i = 0; i < times.length; i++) {
+      let possibilitiesCount = 0
+      const timesCount = +times[i]
+      console.log(timesCount)
+      const distancesCount = +distances[i]
+      for (let j = 0; j < timesCount; j++) {
+        const timeLeft = timesCount - j;
+        const distanceLeft = timeLeft * j;
+        if (distanceLeft > distancesCount) {
+          possibilitiesCount++
+        }
+      }
+      multi *= possibilitiesCount
+    }
+    // console.log(multi)
